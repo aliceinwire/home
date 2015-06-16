88a86e6420ad767a37155186cf43574dfc1af8ae
Alice Ferrazzi <alice.ferrazzi@gmail.com>
2015-06-16 22:45:57 +0900

    Added i3 configuration

---
+++ home/.i3/pystatus.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#import subprocess
+
+from i3pystatus import Status
+
+status = Status(standalone=True)
+
+# Displays clock like this:
+# Tue 30 Jul 11:59:46 PM KW31
+#                          ^-- calendar week
+status.register("clock", format="%a %-d %b %X KW%V",)
+
+# Shows the average load of the last minute and the last 5 minutes
+# (the default value for format is used)
+status.register("load")
+
+# Shows your CPU temperature, if you have a Intel CPU
+status.register("temp", format="{temp:.0f}°C",)
+
+status.run()
